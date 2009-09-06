bb39c6cabcd2e194da2d14dbc7cb97d7c8b2250e
netro82 <netro82@gmail.com>
2009-09-06 11:49:10 +0200

    Changed loading of jquery, deleted jquery from project, add somer contstrucion to pmdr.js

--- js/pmdr.js
+++ js/pmdr.js
@@ -3,3 +3,23 @@
 // Date: 3.9.2009
 // used libraries: jquery.js
 
+  //I am not quite sure with this construction :), realy extend jQuery? 
+  var pomodoroTimer = jQuery.extend({
+    init: function(){
+     // this.state = 'stopped';
+    },
+    start: function(){        
+      this.state = 'running';
+    },
+    stop: function(){        
+      this.state = 'stopped';
+    },
+    state: 'stopped'
+  }); 
+  
+  $(document).ready(function () {
+    $('#button_start').bind('click',function(){ pomodoroTimer.start(); alert(pomodoroTimer.state); });                 
+    $('#button_interruption').bind('click',function(){ pomodoroTimer.stop();alert(pomodoroTimer.state); });
+  });//document ready
+ 
+ 
