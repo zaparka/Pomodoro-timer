15c02a506c939838533f8fb904d03b424c6807dd
ladislav.martincik <ladislav.martincik@xing.com>
2009-09-12 16:28:08 +0200

    Added ClietSideStorage. But it's not working fully because of async javascript calls.

--- js/pmdr.js
+++ js/pmdr.js
@@ -3,36 +3,88 @@
 // Date: 3.9.2009
 // used libraries: jquery.js
 
-function HtmlStorage() {
+function ClientSideStorage() {
   if (window.openDatabase){
-    this.database = openDatabase("pomodoro_timer", "1.0", "HTML5 Database for PomodoroTimer", 200000);
-    if (!this.database)
+    this.storage = openDatabase("pomodoro_timer", "1.0", "HTML5 Database for PomodoroTimer", 200000);
+    if (!this.storage)
       alert("Failed to open the database on disk.");
-  } else
+  } else {
     alert("Couldn't open the database.  Please try with a WebKit nightly with this feature enabled");
+  }
+  
+  this.init();
+  // this.storage.transaction(function(request) {
+  //   request.executeSql("DROP TABLE tasks", [], function(req, result) {}, 
+  //     function(error) {
+  //       alert(error.message);
+  //     } );
+  // });
 }
 
-HtmlStorage.prototype = {
-  database: null,
+ClientSideStorage.prototype = {
+  storage: null,
   
   init: function() {
-    this.database.transaction(function(request) {
-      request.executeSql("SELECT COUNT(*) FROM tasks", [], function(result) {
-        loadNotes();
-      },
+    this.storage.transaction(function(request) {
+      request.executeSql("SELECT COUNT(*) FROM tasks", [], function(result) {},
       
-      function(tx, error) {
-        request.executeSql("CREATE TABLE tasks (id INT, name TEXT, pomodoros INT, interruptions INT)", [],
-          function(result) { 
-            loadNotes(); 
+      function(request, error) {
+        request.executeSql("CREATE TABLE tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, number_of_pomodoros INT, number_of_interuptions INT)", [],
+          function(result, error) { 
+            //alert(error);
           });
       });
     });
   },
+  
   insert: function(task) {
-    db.transaction(function (request){
-      request.executeSql("INSERT INTO tasks (id, name, pomodoros, interruptions) VALUES (?, ?, ?, ?)", [task.id, task.name, task.pomodoro, task.interruption]);
-    });  
+    this.storage.transaction(function(request) {
+      request.executeSql("INSERT INTO tasks (name, number_of_pomodoros, number_of_interuptions) VALUES (?, ?, ?)", 
+       [task.name, task.number_of_pomodoros, task.number_of_interuptions], function(result) {}, 
+       function(request, error) {
+         alert(error.message);
+         return;
+       });
+    });
+  },
+  
+  update: function(id, task) {
+    this.storage.transaction(function(request) {
+      request.executeSql("UPDATE tasks SET name = ?, number_of_pomodoros = ?, number_of_interuptions = ? LIMIT 1 OFFSET ?", 
+        [task.name, task.number_of_pomodoros, task.number_of_interuptions]);
+    });
+  },
+  
+  list: function() {
+    var tasks = new Array();
+    var wait4result = true;
+    
+    this.storage.transaction(function(request) {
+      request.executeSql("SELECT * FROM tasks", [], function(req, result) {
+        for (var i = 0; i < result.rows.length; ++i) {
+          var row = result.rows.item(i);
+          var task = new Task();
+          task.id = row['id'];
+          task.name = row['name'];
+          task.number_of_pomodoros = row['number_of_pomodoros'];
+          task.number_of_interuptions = row['number_of_interuptions'];
+
+          tasks.push(task);
+        }
+
+        wait4result = false;
+      }, function(tx, error) {
+          wait4result = false;
+          alert('Failed to retrieve notes from database - ' + error.message);
+          return;
+        }
+      );
+      
+    });
+    
+    while (wait4result) {  };
+    
+    return tasks;
   }
   
 };
@@ -123,7 +175,7 @@ function Task(name) {
 
 Task.prototype = {
   name: '',
-  number_of_pomodoro: 0,
+  number_of_pomodoros: 0,
   number_of_interuptions: 0
 };
 
@@ -233,8 +285,8 @@ TimeFormatter = {
   
 $(document).ready(function () {
   var pomodoroTimer = new PomodoroTimer();
-  var mem = new MemoryStorage();
-  var taskManager = new TaskManager(mem  );
+  var taskManager = new TaskManager(new MemoryStorage());
+  taskManager.updateHTMLSelect();
   $('#button_start').bind('click',function(){ pomodoroTimer.start(); });
   $('#button_interruption').bind('click',function(){ pomodoroTimer.stop(); });
   $('#button_add').bind('click', function(){ taskManager.addTask(); });
