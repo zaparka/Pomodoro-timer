40f98d965d7a49604a983911fbd84a796fa05800
netro82 <netro82@gmail.com>
2009-09-19 17:45:02 +0200

    added Rakefile, task and template file

---
+++ Rakefile
@@ -0,0 +1,8 @@
+require 'dm-core'
+require 'pomodoro'
+require 'task'
+
+desc "Migrate database"
+task :migrate do
+  DataMapper.auto_migrate!
+end
---
+++ task.rb
@@ -0,0 +1,7 @@
+class Task
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, String
+  property :number_of_pomodoros, Integer
+  property :number_of_interuptions, Integer
+end
