5267fb1a38d6e7b769e6b1a4d2f42a981e7f93db
netro82 <netro82@gmail.com>
2009-09-19 16:28:14 +0200

    added insert metod, and tests for check inserted data, new  rakefile and new task.rb file

--- pomodoro.rb
+++ pomodoro.rb
@@ -1,9 +1,22 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
-require 'sass'
+#require 'sass'
 require 'dm-core'
 require 'dm-validations'
 require 'dm-aggregates'
-require 'dm-more'
+#require 'dm-more'
 require 'fileutils'
+require 'task'
+
+DataMapper::setup(:default, {
+  :adapter  => "mysql",
+  :database => "pomodoro",
+  :username => "root",
+  :password => "",
+  :host     => "localhost"
+})
+
+post '/' do
+  Task.create(params) 
+end
--- pomodoro_test.rb
+++ pomodoro_test.rb
@@ -13,15 +13,24 @@ class PomodoroTest < Test::Unit::TestCase
   end
   
   def setup
-    #Task.all.destroy!
+    Task.all.destroy!
   end
 
-  def test_it_inserts_task_to_database
-    #pre_count = Task.count
+  def test_if_inserts_task_to_database
+    pre_count = Task.count
     post '/', :name => 'Name of the task', :number_of_pomodoros => 1, :number_of_interuptions => 5
     
     assert last_response.ok?
-    #assert pre_count + 1, Task.count
+    assert pre_count + 1, Task.count
+  end
+  
+  def test_if_inserts_right_data_for_task  
+    post '/', :name => 'Name of the task', :number_of_pomodoros => 1, :number_of_interuptions => 5
+    
+    assert last_response.ok?
+    assert 'Name of the task',Task.last.name
+    assert 1,Task.last.number_of_pomodoros
+    assert 5,Task.last.number_of_interuptions 
   end
   
 end
